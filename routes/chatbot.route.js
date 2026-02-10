19d0864a9f0185afffa81fba535b4c49b55d90a4
Shlok Mishra <shlokmishra576@gmail.com>
2026-02-10 22:51:00 +0530

    Fixed chatbot route at /bot/v1/message

--- routes/chatbot.route.js
+++ routes/chatbot.route.js
@@ -2,7 +2,15 @@ import  express from "express"
 import { Message } from "../Controllers/chatbot.message.js"
 
 const router = express.Router()
+router.post("/message", (req, res) => {
+  const userMessage = req.body.text;
+  const botMessage = `You said: ${userMessage}`;
 
-router.post("/message" ,Message)
+  res.json({
+    userMessage,
+    botMessage,
+  });
+});
+
 
 export default router
